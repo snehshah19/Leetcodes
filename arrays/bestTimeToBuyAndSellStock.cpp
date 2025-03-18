e45a6878e58de4c39509cd026d3de6edab84c88d
sneh shah <snehshah1903@gmail.com>
2025-03-18 16:08:12 +0530

    new questions added

---
+++ arrays/bestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+    public:
+        int maxProfit(vector<int>& prices) {
+            int maxProfit = 0;
+            int bestBuy = prices[0];
+            
+            for(int i = 1;i<prices.size();i++){
+                if(prices[i] > bestBuy){
+                    maxProfit = max(maxProfit,prices[i] - bestBuy);
+                }
+                bestBuy = min(bestBuy,prices[i]);
+            }   
+            return maxProfit;
+        }
+    };
